2051e6bef94ee105ac0c0d635a50ea9d4bad30ee
saumya-bansal <saumzz19@gmail.com>
2022-04-21 07:31:37 -0600

    fixed analyze priority

--- worker/worker_function.py
+++ worker/worker_function.py
@@ -4,9 +4,10 @@ def analyze_priority(color, description):
 
     priority = 0
 
+    priority = colorMarker.get(color.lower())
+
     for word in keywords:
         if word in description.lower():
             priority = 1
 
-    priority += colorMarker.get(color.lower())
     return priority
